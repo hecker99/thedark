50355807a0ed794504a20f143288db5aae519183
hecker99 <43149943+hecker99@users.noreply.github.com>
2019-07-14 23:33:41 +0530

    Add files via upload

--- aa.java
+++ aa.java
@@ -1,3 +1,5 @@
+
+
 import java.util.Scanner;
 
 class a{
@@ -10,28 +12,46 @@ class a{
 		int b = s.nextInt();
 		int c = s.nextInt();
 
-		if ((a>b) && (b>c) && (a>c)){
-			System.out.println("third largest number is : " + b );
+if ((a>b) && (b>c)){
+	System.out.println("third largest number is : " + c );
+	
+	}
+	
+ else if  ((b>a) && (c>a)){
+	 
+			System.out.println("third largest number is : " + a);	}
+
+ else if  ((a>c) && (b>c)){
+	 
+		System.out.println("third largest number is : " + c);	}	
+
+ else if  ((a>b) && (c>b)){
+	 
+		System.out.println("third largest number is : " + b);	}
+	 }
+	}
+
 			
-			}
+	
+
+	
+
+				
 			
-		 else if  ((b>c) && (c>a) && (b>a)){
-			 
-					System.out.println("third largest number is : " + c);	}	
-		 else if  ((b<c) && (c>a) && (a>b)){
-			 
-				System.out.println("third largest number is : " + a);	}	
+				
+	
 
-		 else if  ((b<c) && (c>a) && (a<b)){
-			 
-				System.out.println("third largest number is : " + b);	}
 
-		 else if  ((b>c) && (b>a) && (a>c)){
-			 
-				System.out.println("third largest number is : " + a);	}
 
-		 else if  ((a>b) && (c>b) && (a>c)){
-			 
-				System.out.println("third largest number is : " + c); }
-			 }
-			}
+
+			
+	
+
+	
+
+				
+			
+				
+	
+
+
