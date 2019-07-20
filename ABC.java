865a7375d76f5328ff8e43f4f3d14655025a898b
hecker99 <43149943+hecker99@users.noreply.github.com>
2019-07-20 11:40:06 +0530

    Add files via upload

---
+++ ABC.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class array {
+
+	public static void main(String[] args) {
+		int  arr [] []= new int [3][3]; 
+		int i,j,row,col;
+		Scanner s = new Scanner (System.in);
+		
+		System.out.print("enter num :"  );
+		for (i=0;i<3;i++) ;{
+			for (j=0;j<3;j++);
+			
+			arr [i] [j] = s.nextInt();
+			
+			
+	}
+		
+		System.out.print("2d array is :" );
+		
+		
+			for ( row=0;row <3; row++) {
+				for ( col=0;col<3;col++);
+				
+				System.out.print(arr[row][col]);
+				
+				
+			}
+		}
+}
+
+		
